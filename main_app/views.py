efc14a3c0f509fc3a0c6efd59145df64a7194776
Joe Gerhard <jlgerhard@gmail.com>
2019-09-10 19:45:24 -0500

    add base template, home page, connect style.css and main.js to base.html template

--- main_app/views.py
+++ main_app/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('Hello world')
+    return render(request, 'main_app/home.html')
