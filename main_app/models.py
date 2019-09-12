06e8361493d2970daafa3c472a9e5f67fdd7bbd6
Joe Gerhard <jlgerhard@gmail.com>
2019-09-11 22:08:20 -0500

    Add models and make migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,39 @@
 from django.db import models
+from django.contrib.postgres.fields import ArrayField
+from django.shortcuts import reverse
 
-# Create your models here.
+class User(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("user_detail", kwargs={"pk": self.pk})
+
+class Template(models.Model):
+    name = models.CharField(max_length=50)
+    blanks = ArrayField(models.CharField(max_length=50))
+    text = models.TextField()
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse("template_detail", kwargs={"pk": self.pk})
+    
+
+class Madlib(models.Model):
+    name = models.CharField(max_length=50)
+    words = ArrayField(models.CharField(max_length=50))
+    text = models.TextField()
+    date_completed = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("madlib_detail", kwargs={"pk": self.pk})
+    
+    
