873113229710258f52c75da956a9ff9dee6dfd15
Joe Gerhard <jlgerhard@gmail.com>
2019-09-12 08:09:17 -0500

    Register models in admin

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import User, Template, Madlib
 
 # Register your models here.
+
+admin.site.register(User)
+admin.site.register(Template)
+admin.site.register(Madlib)
