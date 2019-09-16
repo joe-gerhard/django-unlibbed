783396031bd440f0447198cd5d94fd925028def1
Frances Cancio <frances.cancio@gmail.com>
2019-09-16 16:17:21 -0500

    create about.html template and url path

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('template/about', views.about, name='about'),
     path('template/', views.TemplateListView.as_view(), name='template_index'),
     path('template/<int:pk>/', views.TemplateDetailView.as_view(), name='template_detail'),
     path('madlib/new/', views.madlib_new_view, name='madlib_new'),
